2b4e9d145d8ca2f5712f75edaf3da6d87704d3b5
ArmainAP <adrian.popoviciu@katcodelabs.com>
2024-02-17 19:26:56 +0200

    *Refactored MFs into OcclusionPortal.ush

--- Source/Portals/Portals.Build.cs
+++ Source/Portals/Portals.Build.cs
@@ -38,6 +38,7 @@ public class Portals : ModuleRules
 				"Engine",
 				"Slate",
 				"SlateCore",
+				"RenderCore"
 				// ... add private dependencies that you statically link with here ...	
 			}
 			);
--- Source/Portals/Private/Portals.cpp
+++ Source/Portals/Private/Portals.cpp
@@ -6,13 +6,13 @@
 
 void FPortalsModule::StartupModule()
 {
-	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+	const FString ShaderDirectory = FPaths::Combine(FPaths::ProjectPluginsDir(), "Unreal-Engine-Portals", TEXT("Shaders"));
+	AddShaderSourceDirectoryMapping("/Portals", ShaderDirectory);
 }
 
 void FPortalsModule::ShutdownModule()
 {
-	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
-	// we call this function before unloading the module.
+	ResetAllShaderSourceDirectoryMappings();
 }
 
 #undef LOCTEXT_NAMESPACE
